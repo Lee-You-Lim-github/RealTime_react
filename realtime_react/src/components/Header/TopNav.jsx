a3a601fb840e770436f9d1375223d9e77e882708
JunSoo Lee <ljsay1113@naver.com>
2022-02-09 18:45:35 +0900

    TopNav에 로그인 여부 반영 및 로그인 여부에 따른 예약현황

--- realtime_react/src/components/Header/TopNav.jsx
+++ realtime_react/src/components/Header/TopNav.jsx
@@ -1,13 +1,13 @@
-// import { useAuth } from "contexts/AuthContext";
+import { useAuth } from "contexts/AuthContext";
 import { NavLink } from "react-router-dom";
 import "./TopNav.css";
 
 function TopNav() {
-  // const [auth, , , logout] = useAuth;
+  const [auth, , , logout] = useAuth();
 
-  // const handleLogout = () => {
-  //   logout();
-  // };
+  const handleLogout = () => {
+    logout();
+  };
 
   return (
     <div className="bg-red-200">
@@ -16,10 +16,21 @@ function TopNav() {
           <NavLink to="/" className="px-4 py-3 mt-2 text-4xl">
             지금어때
           </NavLink>
-
           <div className="flex text-xl mr-2">
-            <MyLink to="/booking/">예약현황</MyLink>
-            <MyLink to="/accounts/login/">로그인</MyLink>
+            {!auth.isLoggedIn && (
+              <>
+                <MyLink to="/accounts/login/">예약현황</MyLink>
+                <MyLink to="/accounts/login/">로그인</MyLink>
+              </>
+            )}
+            {auth.isLoggedIn && (
+              <>
+                <MyLink to="/booking/">예약현황</MyLink>
+                <button onClick={handleLogout} className={baseClassName}>
+                  로그아웃
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
@@ -43,60 +54,3 @@ function MyLink({ to, children }) {
 const baseClassName = "px-4 pt-6 mt-4 font-semibold hover:text-red-500";
 
 export default TopNav;
-
-// import { useAuth } from "contexts/AuthContext";
-// import { NavLink } from "react-router-dom";
-// import "./TopNav.css";
-
-// function TopNav() {
-//   const [auth, , , logout] = useAuth;
-
-//   const handleLogout = () => {
-//     logout();
-//   };
-
-//   return (
-//     <div className="bg-red-200">
-//       <div className="header">
-//         <div className="flex place-content-between gap-3 border-b-4 border-red-300">
-//           <NavLink to="/" className="px-4 py-3 mt-2 text-4xl">
-//             지금어때
-//           </NavLink>
-//           <div className="flex text-xl mr-2">
-//             {!auth.isLoggedIn && (
-//               <>
-//                 <MyLink to="/booking/">예약현황</MyLink>
-//                 <MyLink to="/accounts/login/">로그인</MyLink>
-//               </>
-//             )}
-//             {auth.isLoggedIn && (
-//               <>
-//                 <MyLink to="/booking/">예약현황</MyLink>
-//                 <button onClick={handleLogout} className={baseClassName}>
-//                   로그아웃
-//                 </button>
-//               </>
-//             )}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// function MyLink({ to, children }) {
-//   return (
-//     <NavLink
-//       to={to}
-//       className={({ isActive }) =>
-//         baseClassName + " " + (isActive ? "border-b-4 border-red-400" : "")
-//       }
-//     >
-//       {children}
-//     </NavLink>
-//   );
-// }
-
-// const baseClassName = "px-4 pt-6 mt-4 font-semibold hover:text-red-500";
-
-// export default TopNav;
