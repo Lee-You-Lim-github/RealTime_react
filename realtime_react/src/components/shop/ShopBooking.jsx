7437393ed7a7147ab36619b14815dad6546db207
Lee-You-Lim-github <akfl513@naver.com>
2022-02-14 13:19:23 +0900

    visit_status만 수정 구현

--- realtime_react/src/components/shop/ShopBooking.jsx
+++ realtime_react/src/components/shop/ShopBooking.jsx
@@ -1,8 +1,334 @@
+import { useApiAxios } from "api/base";
+import DebugStates from "components/DebugStates";
+import { useAuth } from "contexts/AuthContext";
+import useFieldValues from "hook/usefieldValues";
+import { useCallback, useEffect, useState } from "react";
+
 function ShopBooking() {
+  const [auth] = useAuth();
+
+  //disabled
+  const [visited, setVisited] = useState(false);
+
+  // get_bookings
+  const [
+    {
+      data: getBookingData,
+      loading: getBookingLoading,
+      error: getBookingError,
+    },
+    refetch,
+  ] = useApiAxios(
+    {
+      url: "/booking/api/bookings/",
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${auth.access}`,
+      },
+    },
+    { manual: true }
+  );
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  // booking - visit_status만 수정
+  const [{ loading, error }, saveBookingVisitState] = useApiAxios(
+    {
+      url: `/booking/api/bookings/1/`,
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${auth.access}`,
+      },
+    },
+    { manual: true }
+  );
+
+  // 회원이 방문한 경우
+  const clickedVisit = useCallback((e) => {
+    saveBookingVisitState({
+      data: { visit_status: "1" },
+    })
+      .then((response) => {
+        console.log(response.data.visit_status);
+        refetch();
+        setVisited(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  // 회원이 미방문한 경우
+  const clickedUnvisited = useCallback((e) => {
+    saveBookingVisitState({
+      data: { visit_status: "2" },
+    })
+      .then((response) => {
+        console.log(response.data.visit_status);
+        refetch();
+        setVisited(false);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div>
-      <h2>예약현황</h2>
-      <h5>예약자명단</h5>
+      <DebugStates getBookingData={getBookingData} auth={auth} />
+
+      <div class="bg-white p-8 rounded-md w-full">
+        <div class=" flex items-center justify-between pb-6">
+          <div>
+            <h2 class="text-gray-600 font-semibold">예약현황</h2>
+            <span class="text-xs">예약자명단</span>
+          </div>
+          <div class="flex items-center justify-between">
+            <div class="flex bg-gray-50 items-center p-2 rounded-md">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5 text-gray-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              <input
+                class="bg-gray-50 outline-none ml-1 block "
+                type="text"
+                name=""
+                id=""
+                placeholder="search..."
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          {getBookingData && (
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+              <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                <table class="min-w-full leading-normal">
+                  <thead>
+                    <tr>
+                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        No.
+                      </th>
+                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        이름
+                      </th>
+                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        휴대폰 번호
+                      </th>
+                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        예약날짜
+                      </th>
+                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        예약 테이블 수
+                      </th>
+                      <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                        방문여부
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */}
+                    <tr>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <div class="flex items-center">
+                          <div class="ml-3">
+                            {getBookingData.map((shop_bookings) => (
+                              <p class="text-gray-900 whitespace-no-wrap">
+                                {shop_bookings.id}
+                              </p>
+                            ))}
+                          </div>
+                        </div>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {getBookingData.map((shop_bookings) => (
+                          <p class="text-gray-900 whitespace-no-wrap">
+                            {shop_bookings.user_id.username}
+                          </p>
+                        ))}
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {getBookingData.map((shop_bookings) => (
+                          <p class="text-gray-900 whitespace-no-wrap">
+                            {shop_bookings.user_id.telephone}
+                          </p>
+                        ))}
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {getBookingData.map((shop_bookings) => (
+                          <p class="text-gray-900 whitespace-no-wrap">
+                            {shop_bookings.day}
+                          </p>
+                        ))}
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        {getBookingData.map((shop_bookings) => (
+                          <p class="text-gray-900 whitespace-no-wrap">
+                            {shop_bookings.time.slice(0, 5)}
+                          </p>
+                        ))}
+                      </td>
+                      <td class="p-3 px-5 flex justify-end">
+                        <button
+                          type="button"
+                          onClick={clickedVisit}
+                          disabled={false}
+                          class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+                        >
+                          방문
+                        </button>
+                        <button
+                          type="button"
+                          onClick={clickedUnvisited}
+                          disabled={false}
+                          class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+                        >
+                          미방문
+                        </button>
+                      </td>
+                    </tr>
+                    {/* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */}
+                    <tr>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <div class="flex items-center">
+                          <div class="flex-shrink-0 w-10 h-10">
+                            <img
+                              class="w-full h-full rounded-full"
+                              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                              alt=""
+                            />
+                          </div>
+                          <div class="ml-3">
+                            <p class="text-gray-900 whitespace-no-wrap">
+                              Vera Carpenter
+                            </p>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">Admin</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">
+                          Jan 21, 2020
+                        </p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">43</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                          <span
+                            aria-hidden
+                            class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+                          ></span>
+                          <span class="relative">Activo</span>
+                        </span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <div class="flex items-center">
+                          <div class="flex-shrink-0 w-10 h-10">
+                            <img
+                              class="w-full h-full rounded-full"
+                              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                              alt=""
+                            />
+                          </div>
+                          <div class="ml-3">
+                            <p class="text-gray-900 whitespace-no-wrap">
+                              Vera Carpenter
+                            </p>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">Admin</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">
+                          Jan 21, 2020
+                        </p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">43</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                          <span
+                            aria-hidden
+                            class="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+                          ></span>
+                          <span class="relative">Activo</span>
+                        </span>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td class="px-5 py-5 bg-white text-sm">
+                        <div class="flex items-center">
+                          <div class="flex-shrink-0 w-10 h-10">
+                            <img
+                              class="w-full h-full rounded-full"
+                              src="https://images.unsplash.com/photo-1522609925277-66fea332c575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&h=160&w=160&q=80"
+                              alt=""
+                            />
+                          </div>
+                          <div class="ml-3">
+                            <p class="text-gray-900 whitespace-no-wrap">
+                              Alonzo Cox
+                            </p>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="px-5 py-5 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">Admin</p>
+                      </td>
+                      <td class="px-5 py-5 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">
+                          Jan 18, 2020
+                        </p>
+                      </td>
+                      <td class="px-5 py-5 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap">70</p>
+                      </td>
+                      <td class="px-5 py-5 bg-white text-sm">
+                        <span class="relative inline-block px-3 py-1 font-semibold text-red-900 leading-tight">
+                          <span
+                            aria-hidden
+                            class="absolute inset-0 bg-red-200 opacity-50 rounded-full"
+                          ></span>
+                          <span class="relative">Inactive</span>
+                        </span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+                  <span class="text-xs xs:text-sm text-gray-900">
+                    Showing 1 to 4 of 50 Entries
+                  </span>
+                  <div class="inline-flex mt-2 xs:mt-0">
+                    <button class="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-l">
+                      Prev
+                    </button>
+                    &nbsp; &nbsp;
+                    <button class="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-r">
+                      Next
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
--- realtime_react/src/pages/shop/PageShopBooking.jsx
+++ realtime_react/src/pages/shop/PageShopBooking.jsx
@@ -1,6 +1,8 @@
 import ShopBooking from "components/shop/ShopBooking";
+import { useParams } from "react-router-dom";
 
 function PageShopBooking() {
+  useParams();
   return (
     <div>
       <ShopBooking />
