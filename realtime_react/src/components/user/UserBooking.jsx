b727504153cfa71d3cb4f252e447a312d7e4778e
Eunji Nam <godangel31@gmail.com>
2022-02-15 17:35:43 +0900

    User 예약현황 구현

--- realtime_react/src/components/user/UserBooking.jsx
+++ realtime_react/src/components/user/UserBooking.jsx
@@ -1,50 +1,96 @@
 import { useApiAxios } from "api/base";
 import DebugStates from "components/DebugStates";
 import { useAuth } from "contexts/AuthContext";
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-function UserBooking() {
+function UserBooking({ userId }) {
+  const navigate = useNavigate();
+
   const [auth] = useAuth();
 
-  const [{ data: bookingList, loading, error }, refetch] = useApiAxios({
-    url: "/booking/api/bookings/",
-    method: "GET",
-  });
+  const [bookingArray, setBookingArray] = useState([]);
+
+  // userId === bookingList.user_id.id 예약만 보여주기
+
+  const [{ data: bookingList, loading, error }, refetch] = useApiAxios(
+    {
+      url: "/booking/api/bookings/",
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${auth.access}`,
+      },
+    },
+    { manual: true }
+  );
 
   useEffect(() => {
     refetch();
-  }, [auth]);
+  }, []);
+
+  // const [{ loading: deleteLoading, error: deleteError }, deleteBooking] =
+  //   useApiAxios(
+  //     {
+  //       url: `/booking/api/bookings/${bookingId}/`,
+  //       method: "DELETE",
+  //       headers: {
+  //         Authorization: `Bearer ${auth.access}`,
+  //       },
+  //     },
+  //     { manual: true }
+  //   );
+
+  // const handleDelete = () => {
+  //   if (window.confirm("Are you sure?")) {
+  //     deleteBooking().then(() => navigate(`/user/bookings/${auth.id}`));
+  //   }
+  // };
+
+  useEffect(() => {
+    const userBooking = bookingList?.filter(
+      (booking) => parseInt(auth.id) === booking.user_id.id
+    );
+
+    setBookingArray(userBooking);
+  }, [bookingList]);
 
   return (
     <div>
-      <h2 className="mb-3">예약현황</h2>
-      {bookingList &&
-        bookingList.map((booking) => (
-          <div key={booking.id} className="mb-3">
-            <Link
-              to={`/shop/${booking.shop_id}/`}
-              className="text-left text-2xl"
-            >
-              {booking.shop_id.name}
-            </Link>
-            <div className="flex flex-wrap">
-              <div className="bg-violet-300 border border-violet-400 text-left w-1/5 rounded-sm p-3">
-                <p>예약날짜</p>
-                <p>예약시간</p>
-                <p>예약 테이블 수</p>
-              </div>
-              <div className="bg-violet-200 border border-violet-400 text-left w-3/5 rounded-sm p-3">
-                <p>{booking.day}</p>
-                <p>{booking.time}</p>
-                <p>{booking.book_table_count}</p>
-                <button className="hover:text-violet-400">예약취소</button>
-              </div>
+      <h2 className="my-3 text-left">예약현황</h2>
+      {bookingArray?.map((booking) => (
+        <div key={booking.id} className="mb-3">
+          <Link to={`/shop/${booking.shop_id.id}/`} className="text-2xl">
+            <p className="text-left">{booking.shop_id.name}</p>
+          </Link>
+          <div className="flex flex-wrap">
+            <div className="bg-violet-300 border border-violet-400 w-1/5 text-left rounded-sm p-3">
+              <p>예약날짜</p>
+              <p>예약시간</p>
+              <p>예약 테이블 수</p>
+            </div>
+            <div className="bg-violet-200 border border-violet-400 w-3/5 rounded-sm p-3">
+              <p className="text-left">{booking.day}</p>
+              <p className="text-left">{booking.time}</p>
+              <p className="text-left">{booking.book_table_count}</p>
+              {/* <button
+                  disabled={deleteLoading}
+                  onClick={handleDelete}
+                  className="bg-violet-300 hover:bg-red-200 text-sm text-right rounded p-1"
+                >
+                  예약취소
+                </button> */}
             </div>
           </div>
-        ))}
+        </div>
+      ))}
+      {!bookingList && "예약 내역이 없습니다."}
 
-      <DebugStates data={bookingList} loading={loading} error={error} />
+      <DebugStates
+        bookingList={bookingList}
+        bookingArray={bookingArray}
+        loading={loading}
+        error={error}
+      />
     </div>
   );
 }
