55519ea21d9d90e8e9c3fe9b0afc4281af18a7c8
Lee-You-Lim-github <akfl513@naver.com>
2022-02-17 14:22:35 +0900

    관리자 회원 관리 페이지 기본 구현

---
+++ realtime_react/src/components/admin/AdminUser.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AdminUser() {
+  return (
+    <div>
+      <h3>회원 관리 게시판</h3>
+    </div>
+  );
+}
+
+export default AdminUser;
---
+++ realtime_react/src/pages/admin/PageAdminUser.jsx
@@ -0,0 +1,13 @@
+import AdminUser from "components/admin/AdminUser";
+import React from "react";
+
+function PageAdminUser() {
+  return (
+    <div>
+      <h2>회원 관리페이지</h2>
+      <AdminUser />
+    </div>
+  );
+}
+
+export default PageAdminUser;
