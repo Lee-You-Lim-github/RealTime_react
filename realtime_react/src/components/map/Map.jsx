dc18844f75de1df2631ef2953ed1ac2d30f1cf40
JunSoo Lee <ljsay1113@naver.com>
2022-02-17 16:42:07 +0900

    휴일 마커 구현

--- realtime_react/src/components/map/Map.jsx
+++ realtime_react/src/components/map/Map.jsx
@@ -10,9 +10,13 @@ import marker4 from "assets/img/marker4.png";
 import marker5 from "assets/img/marker5.png";
 import { useApiAxios } from "api/base";
 import DebugStates from "components/DebugStates";
+import { CustomOverlayMap } from "react-kakao-maps-sdk";
+import { data } from "autoprefixer";
 
 function Map({ getData }) {
   const navigate = useNavigate();
+
+  const [isOpen, setIsOpen] = useState(false);
 
   const shopObject = Object.assign({}, getData);
   console.log("shopObject:", shopObject);
@@ -38,33 +42,41 @@ function Map({ getData }) {
         total_table_count: data.total_table_count,
         photo: data.photo,
         telephone: data.telephone,
+        holiday: data.holiday,
         // rating: data.rating,
       };
     });
 
     for (var i = 0; i < positions.length; i++) {
       var imageSize = new kakao.maps.Size(44, 55);
-      if (
-        (positions[i].now_table_count / positions[i].total_table_count) * 100 <
-        33
-      ) {
-        var markerImage = new kakao.maps.MarkerImage(marker1, imageSize);
-      } else if (
-        (positions[i].now_table_count / positions[i].total_table_count) * 100 <
-        66
-      ) {
-        var markerImage = new kakao.maps.MarkerImage(marker2, imageSize);
-      } else if (
-        (positions[i].now_table_count / positions[i].total_table_count) * 100 <
-        99
-      ) {
-        var markerImage = new kakao.maps.MarkerImage(marker3, imageSize);
-      } else if (
-        (positions[i].now_table_count / positions[i].total_table_count) *
-          100 ===
-        100
-      ) {
-        var markerImage = new kakao.maps.MarkerImage(marker4, imageSize);
+      if (positions[i].holiday === "1") {
+        var markerImage = new kakao.maps.MarkerImage(marker5, imageSize);
+      } else if (positions[i].holiday === "0") {
+        if (
+          (positions[i].now_table_count / positions[i].total_table_count) *
+            100 <
+          33
+        ) {
+          var markerImage = new kakao.maps.MarkerImage(marker1, imageSize);
+        } else if (
+          (positions[i].now_table_count / positions[i].total_table_count) *
+            100 <
+          66
+        ) {
+          var markerImage = new kakao.maps.MarkerImage(marker2, imageSize);
+        } else if (
+          (positions[i].now_table_count / positions[i].total_table_count) *
+            100 <
+          99
+        ) {
+          var markerImage = new kakao.maps.MarkerImage(marker3, imageSize);
+        } else if (
+          (positions[i].now_table_count / positions[i].total_table_count) *
+            100 ===
+          100
+        ) {
+          var markerImage = new kakao.maps.MarkerImage(marker4, imageSize);
+        }
       }
 
       var infoContent =
@@ -72,6 +84,7 @@ function Map({ getData }) {
         '    <div class="info">' +
         '        <div class="title">' +
         `            ${positions[i].name}` +
+        `            <div class="close" onclick="" title="닫기"></div>` +
         "        </div>" +
         '        <div class="body">' +
         '            <div class="img">' +
@@ -81,7 +94,7 @@ function Map({ getData }) {
         `                <div class="ellipsis">${positions[i].address}</div>` +
         // `                <div class="jibun ellipsis">별점 : ${positions[i].rating}</div>` +
         `                <div class="ellipsis">Tel : ${positions[i].telephone}</div>` +
-        `                <div><a href="/shop/${positions[i].shop_id}/">매장 상세보기</a></div>` +
+        `                <div><a href="/shop/${positions[i].shop_id}/" class="link">매장 상세보기</a></div>` +
         "            </div>" +
         "        </div>" +
         "    </div>" +
@@ -115,17 +128,16 @@ function Map({ getData }) {
         "click",
         makeOverListener(map, marker, overlay)
       );
+      // kakao.maps.event.addListener(map, "click", closeOverlay(marker));
     }
     function makeOverListener(map, marker, overlay) {
       return function () {
         overlay.setMap(map, marker, overlay);
       };
     }
-    function closeOverlay(overlay) {
-      return function () {
-        overlay.setMap(null);
-        // marker.setMap(null);
-      };
+    function closeOverlay() {
+      overlay.setMap(null);
+      // marker.setMap(null);
     }
   }, []);
 
