ff05d77f3086361cb3f9502803b2a33a6ede73c0
Lee-You-Lim-github <akfl513@naver.com>
2022-02-17 14:17:52 +0900

    관리자로 로그인 시 관리자 페이지만 보이도록 구현

--- realtime_react/src/App.js
+++ realtime_react/src/App.js
@@ -13,21 +13,24 @@ import PageShopBooking from "pages/shop/PageShopBooking";
 import PageUserBooking from "pages/user/PageUserBooking";
 import { useAuth } from "contexts/AuthContext";
 import PageUserEdit from "pages/user/PageUserEdit";
+import PageAdminShop from "pages/admin/PageAdminShop";
+import PageAdminBooking from "pages/admin/PageAdminBooking";
+import PageAdminUser from "pages/admin/PageAdminUser";
 
 function App() {
   const [auth, , login, logout] = useAuth();
 
   const authority_topnavi = (a) => {
-    if (auth.authority === "0") {
+    if (a === "0") {
       return true;
     } else {
       return false;
     }
   };
 
-  // 유저가 로그인하고 보이는 페이지
   if (auth.isLoggedIn) {
-    if (authority_topnavi(auth.authority)) {
+    // 관리자로 로그인 시
+    if (auth.is_superuser) {
       return (
         <>
           <TopNav />
@@ -36,53 +39,82 @@ function App() {
           </Routes>
           <div className="App">
             <Routes>
-              <Route path="/account/userjoin/" element={<PageUserJoin />} />
-              <Route
-                path="/shop/:shopId/booking/new/"
-                element={<PageBookingForm />}
-              />
-              <Route path="/shop/:shopId/" element={<PageShopDetail />} />
-              <Route path="/user/mypage/:userId/" element={<PageUserInfo />} />
-              <Route
-                path="/user/mypage/:userId/edit/"
-                element={<PageUserEdit />}
-              />
-              <Route
-                path="/user/bookings/:userId/"
-                element={<PageUserBooking />}
-              />
+              <Route path="/admin/user/" element={<PageAdminUser />} />
+              <Route path="/admin/shop/" element={<PageAdminShop />} />
+              <Route path="/admin/booking/" element={<PageAdminBooking />} />
             </Routes>
           </div>
         </>
       );
-    } // 사업자로 로그인 시 보이는 페이지
-    else {
-      return (
-        <>
-          <TopNav />
-          <Routes>
-            <Route path="/" element={<PageIndex />} />
-          </Routes>
-          <div className="App">
+    } else {
+      // 유저가 로그인 시
+      if (authority_topnavi(auth.authority)) {
+        return (
+          <>
+            <TopNav />
             <Routes>
-              <Route path="/shop/new/" element={<PageShopForm />} />
-              <Route path="/shop/:shopId/" element={<PageShopDetail />} />
-              <Route path="/shop/:shopId/edit/" element={<PageShopForm />} />
-              <Route path="/shop/myshop/:shopId/" element={<PageMyShop />} />
-              <Route
-                path="/shop/:shopId/bookings/"
-                element={<PageShopBooking />}
-              />
-              <Route path="/user/mypage/:userId/" element={<PageUserInfo />} />
-              <Route
-                path="/user/mypage/:userId/edit/"
-                element={<PageUserJoin />}
-              />
+              <Route path="/" element={<PageIndex />} />
             </Routes>
-          </div>
-        </>
-      );
+            <div className="App">
+              <Routes>
+                <Route path="/account/userjoin/" element={<PageUserJoin />} />
+                <Route
+                  path="/shop/:shopId/booking/new/"
+                  element={<PageBookingForm />}
+                />
+                <Route path="/shop/:shopId/" element={<PageShopDetail />} />
+                <Route
+                  path="/user/mypage/:userId/"
+                  element={<PageUserInfo />}
+                />
+                <Route
+                  path="/user/mypage/:userId/edit/"
+                  element={<PageUserEdit />}
+                />
+                <Route
+                  path="/user/bookings/:userId/"
+                  element={<PageUserBooking />}
+                />
+              </Routes>
+            </div>
+          </>
+        );
+      }
+      // 사업자로 로그인 시
+      else {
+        return (
+          <>
+            <TopNav />
+            <Routes>
+              <Route path="/" element={<PageIndex />} />
+            </Routes>
+            <div className="App">
+              <Routes>
+                <Route path="/account/userjoin/" element={<PageUserJoin />} />
+                <Route path="/shop/new/" element={<PageShopForm />} />
+                <Route path="/shop/:shopId/" element={<PageShopDetail />} />
+                <Route path="/shop/:shopId/edit/" element={<PageShopForm />} />
+                <Route path="/shop/myshop/:shopId/" element={<PageMyShop />} />
+                <Route
+                  path="/shop/:shopId/bookings/"
+                  element={<PageShopBooking />}
+                />
+                <Route
+                  path="/user/mypage/:userId/"
+                  element={<PageUserInfo />}
+                />
+                <Route
+                  path="/user/mypage/:userId/edit/"
+                  element={<PageUserJoin />}
+                />
+              </Routes>
+            </div>
+          </>
+        );
+      }
     }
+
+    // 비회원인 경우
   } else {
     console.log("===================================");
     console.log(auth.isLoggedIn);
