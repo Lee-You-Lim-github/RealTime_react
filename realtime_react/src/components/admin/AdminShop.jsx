16e89da5020d7ed967f650c38943b5cabc54c905
Lee-You-Lim-github <akfl513@naver.com>
2022-02-17 14:38:34 +0900

    매장 관리페이지 기본 구현

---
+++ realtime_react/src/components/admin/AdminShop.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AdminShop(props) {
+  return (
+    <div>
+      <h3>매장 관리 게시판</h3>
+    </div>
+  );
+}
+
+export default AdminShop;
---
+++ realtime_react/src/pages/admin/PageAdminShop.jsx
@@ -0,0 +1,12 @@
+import AdminShop from "components/admin/AdminShop";
+
+function PageAdminShop() {
+  return (
+    <div>
+      <h1>매장 관리페이지</h1>
+      <AdminShop />
+    </div>
+  );
+}
+
+export default PageAdminShop;
