77dee21398308a97bfaaa592e4303d50326cc65a
Eunji Nam <godangel31@gmail.com>
2022-02-17 15:54:19 +0900

    관리자 예약현황 페이지 구현

--- realtime_react/src/components/admin/AdminBooking.jsx
+++ realtime_react/src/components/admin/AdminBooking.jsx
@@ -1,7 +1,118 @@
+import { useApiAxios } from "api/base";
+import AdminBookingComponent from "components/admin/AdminBookingComponent";
+import { useAuth } from "contexts/AuthContext";
+import { useEffect } from "react";
+
 function AdminBooking() {
+  const [auth] = useAuth();
+
+  const [
+    {
+      data: getBookingData,
+      loading: getBookingLoading,
+      error: getBookingError,
+    },
+    refetch,
+  ] = useApiAxios(
+    {
+      url: `/booking/api/bookings/`,
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${auth.access}`,
+      },
+    },
+    { manual: true }
+  );
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
   return (
-    <div>
-      <h3>예약현황</h3>
+    <div class="bg-white p-8 rounded-md w-full">
+      <div class=" flex items-center justify-between pb-6">
+        <div>
+          <h2 class="text-gray-600 font-semibold">예약현황</h2>
+        </div>
+        <div class="flex items-center justify-between">
+          <div class="flex bg-gray-50 items-center p-2 rounded-md">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5 text-gray-400"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <input
+              class="bg-gray-50 outline-none ml-1 block "
+              type="search"
+              placeholder="매장명/예약자명"
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+          <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+            <table class="min-w-full leading-normal">
+              <thead>
+                <tr>
+                  <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    No.
+                  </th>
+                  <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    사업자등록번호
+                  </th>
+                  <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    매장명
+                  </th>
+                  <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    회원ID
+                  </th>
+                  <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    예약자명
+                  </th>
+                  <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    예약날짜
+                  </th>
+                  <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    예약시간
+                  </th>
+                  <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    예약 테이블 수
+                  </th>
+                  <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    방문여부
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {getBookingData?.map((booking) => {
+                  return <AdminBookingComponent booking={booking} />;
+                })}
+              </tbody>
+            </table>
+            {/* <div class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between          ">
+              <span class="text-xs xs:text-sm text-gray-900">
+                Showing 1 to 4 of 50 Entries
+              </span>
+              <div class="inline-flex mt-2 xs:mt-0">
+                <button class="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-l">
+                  Prev
+                </button>
+                <button class="text-sm text-indigo-50 transition duration-150 hover:bg-indigo-500 bg-indigo-600 font-semibold py-2 px-4 rounded-r">
+                  Next
+                </button>
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
