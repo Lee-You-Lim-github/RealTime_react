db624ec4d53f0ef9abe744c0ceb9b2dd5ec48cf9
Eunji Nam <godangel31@gmail.com>
2022-02-17 10:32:40 +0900

    관리자 예약현황 기본 구현

---
+++ realtime_react/src/pages/admin/PageAdminBooking.jsx
@@ -0,0 +1,11 @@
+import AdminBooking from "components/admin/AdminBooking";
+
+function PageAdminBooking() {
+  return (
+    <div>
+      <AdminBooking />
+    </div>
+  );
+}
+
+export default PageAdminBooking;
