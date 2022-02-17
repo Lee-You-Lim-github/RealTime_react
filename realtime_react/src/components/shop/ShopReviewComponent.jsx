884459d948bc4096cc6db2d7d1e46f10e232c464
SeungAh Kim <cpuasdnwin@naver.com>
2022-02-17 15:20:00 +0900

    ShopDetailPage 기능 구현

---
+++ realtime_react/src/components/shop/ShopReviewComponent.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Star from "./ShopStar";
+import Timestamp from "react-timestamp";
+
+function ShopReviewComponent({ review }) {
+  const { rating, user_id, content } = review;
+  console.log(rating);
+  console.log(user_id);
+  console.log(content);
+
+  return (
+    <React.Fragment>
+      <div className="flex justify-center items-center">
+        <div className="grid grid-cols-4 gap-2">
+          <div className="">
+            <Star score={rating} />
+          </div>
+          <div className="">{user_id.nickname}</div>
+          <div className="">{content}</div>
+          <div>
+            <Timestamp relative date={review.created_at} autoUpdate />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default ShopReviewComponent;
