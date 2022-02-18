f1e23c1bf92994099b28b6a6f3c5df502cec4236
Lee-You-Lim-github <akfl513@naver.com>
2022-02-18 10:39:45 +0900

    코드 정리

--- realtime_react/src/components/shop/ShopBookingComponent.jsx
+++ realtime_react/src/components/shop/ShopBookingComponent.jsx
@@ -4,45 +4,48 @@ function ShopBookingComponent({
   shop_booking,
   clickedVisit,
   clickedUnvisited,
+  loading,
 }) {
   return (
     <React.Fragment>
       <tr>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <div class="flex items-center">
-            <div class="ml-3">
-              <p class="text-gray-900 whitespace-no-wrap">{shop_booking.id}</p>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <div className="flex items-center">
+            <div className="ml-3">
+              <p className="text-gray-900 whitespace-no-wrap">
+                {shop_booking.id}
+              </p>
             </div>
           </div>
         </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p class="text-gray-900 whitespace-no-wrap">
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">
             {shop_booking.user_id.username}
           </p>
         </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p class="text-gray-900 whitespace-no-wrap">
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">
             {shop_booking.user_id.telephone}
           </p>
         </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p class="text-gray-900 whitespace-no-wrap">{shop_booking.day}</p>
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">{shop_booking.day}</p>
         </td>
 
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p class="text-gray-900 whitespace-no-wrap">
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">
             {shop_booking.time.slice(0, 5)}
           </p>
         </td>
-        <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-          <p class="text-gray-900 whitespace-no-wrap">
+        <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+          <p className="text-gray-900 whitespace-no-wrap">
             {shop_booking.book_table_count}
           </p>
         </td>
-        <td class="p-3 px-5 flex justify-end">
+        <td className="p-3 px-5 flex justify-end">
           <button
             type="button"
-            disabled={false}
+            disabled={loading}
             onClick={() => clickedVisit(shop_booking.id)}
             class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
           >
@@ -51,7 +54,7 @@ function ShopBookingComponent({
           <button
             type="button"
             onClick={() => clickedUnvisited(shop_booking.id)}
-            disabled={false}
+            disabled={loading}
             class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
           >
             미방문
