291fd36ff0bf385b1869e1fd65a193a692cfb605
JunSoo Lee <ljsay1113@naver.com>
2022-02-23 15:43:15 +0900

    접근 권한 페이지 구현

--- realtime_react/src/pages/notfound/PageNotAccess.jsx
+++ realtime_react/src/pages/notfound/PageNotAccess.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import warning from "assets/img/warning1.png";
+import { Link } from "react-router-dom";
 
-function PageNotAccess(props) {
-  return <h1>접근 권한이 없습니다.</h1>;
+function PageShopNot() {
+  return (
+    <div className="flex flex-col">
+      <div className="">
+        <img className="w-250 h-50" alt="warn" src={warning} />
+      </div>
+      <div className="text-4xl pt-11">We Are Sorry...</div>
+      <div className="text-xl pt-3">
+        You do not have access rights. Please check your login status.
+      </div>
+      <Link to={`/`} className="text-violet-600 hover:text-red-300 pt-10">
+        Go Back
+      </Link>
+    </div>
+  );
 }
 
-export default PageNotAccess;
+export default PageShopNot;
--- realtime_react/src/pages/notfound/PageShopNot.jsx
+++ realtime_react/src/pages/notfound/PageShopNot.jsx
@@ -10,7 +10,6 @@ function PageShopNot() {
       <div className="text-4xl pt-11">We Are Sorry...</div>
       <div className="text-xl pt-3">Please login as an individual member</div>
       <Link to={`/`} className="text-violet-600 hover:text-red-300 pt-10">
-        {" "}
         Go Back
       </Link>
     </div>
