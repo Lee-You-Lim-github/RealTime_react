e753ccb1a053ff3eaf3774c88d7cb2963ef30e93
JunSoo Lee <ljsay1113@naver.com>
2022-02-23 15:35:30 +0900

    매장 상세 사업자 접근 권한 페이지 구현

--- realtime_react/src/pages/notfound/PageShopNot.jsx
+++ realtime_react/src/pages/notfound/PageShopNot.jsx
@@ -1,5 +1,20 @@
+import warning from "assets/img/warning1.png";
+import { Link } from "react-router-dom";
+
 function PageShopNot() {
-  return <div>개인 회원으로 로그인 해주세요</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="">
+        <img className="w-250 h-50" alt="warn" src={warning} />
+      </div>
+      <div className="text-4xl pt-11">We Are Sorry...</div>
+      <div className="text-xl pt-3">Please login as an individual member</div>
+      <Link to={`/`} className="text-violet-600 hover:text-red-300 pt-10">
+        {" "}
+        Go Back
+      </Link>
+    </div>
+  );
 }
 
 export default PageShopNot;
