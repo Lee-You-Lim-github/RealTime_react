89adf7938ec0b4293048225a2d59ba90cc66b370
Eunji Nam <godangel31@gmail.com>
2022-02-24 10:43:54 +0900

    지금예약 취소시 매장 현재테이블 수 감소되도록 구현

--- realtime_react/src/components/user/UserBooking.jsx
+++ realtime_react/src/components/user/UserBooking.jsx
@@ -1,24 +1,20 @@
 import { useApiAxios } from "api/base";
-import DebugStates from "components/DebugStates";
 import { useAuth } from "contexts/AuthContext";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
 import LoadingIndicator from "components/LoadingIndicator";
-import DeleteConfirmModal from "components/modal/DeleteConfirmModal";
+import DebugStates from "components/DebugStates";
+import UserBookingComponent from "./UserBookingComponent";
 
 function UserBooking() {
   const [auth] = useAuth();
 
   const [bookingArray, setBookingArray] = useState([]);
 
-  // confirm 모달창
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const [bookingDeleteId, setBookingDeleteId] = useState();
-
   // userId === bookingList.user_id.id 예약만 보여주기
-  const [{ data: bookingList, loading, error }, refetch] = useApiAxios(
+  const [
+    { data: bookingList, loading: bookingLoading, error: bookingError },
+    refetch,
+  ] = useApiAxios(
     {
       url: "/booking/api/bookings/",
       method: "GET",
@@ -39,96 +35,23 @@ function UserBooking() {
     }
   }, [bookingList, auth.id]);
 
-  const [{ loading: deleteLoading, error: deleteError }, deleteBooking] =
-    useApiAxios(
-      {
-        url: `/booking/api/bookings/${bookingList?.id}/`,
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${auth.access}`,
-        },
-      },
-      { manual: true }
-    );
-
-  const handleDelete = (e) => {
-    deleteBooking({
-      url: `/booking/api/bookings/${bookingDeleteId}/`,
-      method: "DELETE",
-    });
-    toast.info("🦄 취소되었습니다.", {
-      position: "top-center",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-    window.location.replace(`/user/bookings/${auth.id}/`);
-  };
-
   useEffect(() => {
     refetch();
   }, []);
 
-  // confirm 모달 열기
-  const openModal = (e) => {
-    setModalOpen(true);
-    setBookingDeleteId(e.target.value);
-  };
-
-  // confirm 모달 닫기
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-
   return (
     <div>
       <h2 className="my-3 text-left">예약현황</h2>
-      {loading && <LoadingIndicator>로딩 중...</LoadingIndicator>}
-      {deleteLoading && <LoadingIndicator>취소 중...</LoadingIndicator>}
-      {deleteError?.response?.status >= 400 && (
-        <div className="text-red-400">삭제에 실패했습니다.</div>
-      )}
+      {bookingLoading && <LoadingIndicator>로딩 중...</LoadingIndicator>}
 
       {bookingArray.length > 0 ? (
         <>
-          {bookingArray?.map((booking) => (
-            <div key={booking.id} className="mb-3">
-              <Link to={`/shop/${booking.shop_id.id}/`} className="text-2xl">
-                <p className="text-left">{booking.shop_id.name}</p>
-              </Link>
-              <div className="flex flex-wrap">
-                <div className="bg-violet-300 border border-violet-400 w-1/5 text-left rounded-sm p-3">
-                  <p>예약날짜</p>
-                  <p>예약시간</p>
-                  <p>예약 테이블 수</p>
-                </div>
-                <div className="border border-violet-400 w-3/5 rounded-sm p-3">
-                  <p className="text-left">{booking.day}</p>
-                  <p className="text-left">{booking.time}</p>
-                  <p className="text-left">{booking.book_table_count}</p>
-                  <React.Fragment>
-                    <button
-                      disabled={deleteLoading}
-                      onClick={openModal}
-                      value={booking.id}
-                      className="bg-violet-300 hover:bg-red-200 text-sm text-right rounded p-1"
-                    >
-                      예약취소
-                    </button>
-                    <DeleteConfirmModal
-                      handleDelete={handleDelete}
-                      open={modalOpen}
-                      close={closeModal}
-                      name="user_booking_delete"
-                      header="1시간 전 예약 취소 시 노쇼(No Show)방지 차원으로 서비스 이용이 제한될 수 있습니다."
-                    />
-                  </React.Fragment>
-                </div>
-              </div>
-            </div>
+          {bookingArray?.map((booking_object) => (
+            <UserBookingComponent
+              key={booking_object.id}
+              booking_object={booking_object}
+              bookingList={bookingList}
+            />
           ))}
         </>
       ) : (
---
+++ realtime_react/src/components/user/UserBookingComponent.jsx
@@ -0,0 +1,146 @@
+import { useApiAxios } from "api/base";
+import LoadingIndicator from "components/LoadingIndicator";
+import DeleteConfirmModal from "components/modal/DeleteConfirmModal";
+import { toast } from "react-toastify";
+import { useAuth } from "contexts/AuthContext";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function UserBookingComponent({ bookingList, booking_object }) {
+  const [auth] = useAuth();
+
+  // 현재 테이블 수 값 변경
+  const [tableCount, setTableCount] = useState();
+
+  // 예약 취소
+  const [bookingDeleteId, setBookingDeleteId] = useState();
+
+  // confirm 모달창
+  const [modalOpen, setModalOpen] = useState(false);
+
+  // 매장 현재 테이블 수 변경
+  const [
+    { loading: shopPatchLoading, error: shopPatchError },
+    shopPatchrefetch,
+  ] = useApiAxios(
+    {
+      url: `/shop/api/shops/${booking_object.shop_id.id}/`,
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${auth.access}`,
+      },
+    },
+    { manual: true }
+  );
+
+  useEffect(() => {
+    setTableCount(booking_object.now_table_count);
+  }, [booking_object]);
+
+  // 예약취소
+  const [{ loading: deleteLoading, error: deleteError }, deleteBooking] =
+    useApiAxios(
+      {
+        url: `/booking/api/bookings/${bookingList?.id}/`,
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${auth.access}`,
+        },
+      },
+      { manual: true }
+    );
+
+  console.log(booking_object.book_table_count);
+
+  // const minus = () => {
+  //   setValue((value - booking_object.book_table_count))
+  // }
+
+  const handleDelete = () => {
+    if (booking_object.method === "1") {
+      setTableCount(
+        booking_object.shop_id.now_table_count - booking_object.book_table_count
+      );
+    }
+
+    deleteBooking({
+      url: `/booking/api/bookings/${bookingDeleteId}/`,
+      method: "DELETE",
+    });
+
+    toast.info("🦄 취소되었습니다.", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    window.location.replace(`/user/${auth.id}/bookings/`);
+  };
+
+  useEffect(() => {
+    shopPatchrefetch({ data: { now_table_count: tableCount } })
+      .then((response) => {
+        console.log(response.data);
+        setTableCount(response.data.now_table_count);
+      })
+      .catch((error) => console.log(error));
+  }, [tableCount]);
+
+  // confirm 모달 열기
+  const openModal = (e) => {
+    setModalOpen(true);
+    setBookingDeleteId(e.target.value);
+  };
+
+  // confirm 모달 닫기
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <div className="mb-3">
+      {deleteLoading && <LoadingIndicator>취소 중...</LoadingIndicator>}
+      {deleteError?.response?.status >= 400 && (
+        <div className="text-red-400">삭제에 실패했습니다.</div>
+      )}
+      <Link to={`/shop/${booking_object.shop_id.id}/`} className="text-2xl">
+        <p className="text-left">{booking_object.shop_id.name}</p>
+      </Link>
+      <div className="flex flex-wrap">
+        <div className="bg-violet-300 border border-violet-400 w-1/5 text-left rounded-sm p-3">
+          <p>예약날짜</p>
+          <p>예약시간</p>
+          <p>예약 테이블 수</p>
+        </div>
+        <div className="border border-violet-400 w-3/5 rounded-sm p-3">
+          <p className="text-left">{booking_object.day}</p>
+          <p className="text-left">{booking_object.time}</p>
+          <p className="text-left">{booking_object.book_table_count}</p>
+
+          <React.Fragment>
+            <button
+              disabled={deleteLoading}
+              onClick={openModal}
+              value={booking_object.id}
+              className="bg-violet-300 hover:bg-red-200 text-sm text-right rounded p-1"
+            >
+              예약취소
+            </button>
+            <DeleteConfirmModal
+              handleDelete={handleDelete}
+              open={modalOpen}
+              close={closeModal}
+              name="user_booking_delete"
+              header="1시간 전 예약 취소 시 노쇼(No Show)방지 차원으로 서비스 이용이 제한될 수 있습니다."
+            />
+          </React.Fragment>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserBookingComponent;
