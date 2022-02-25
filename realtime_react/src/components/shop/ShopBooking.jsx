e01712ea61b858469b1bdc8a7ae9692e0ecfe0c2
Lee-You-Lim-github <akfl513@naver.com>
2022-02-25 13:49:04 +0900

    매장 예약현환 css 수정

--- realtime_react/src/components/shop/ShopBooking.jsx
+++ realtime_react/src/components/shop/ShopBooking.jsx
@@ -147,7 +147,7 @@ function ShopBooking({ shopId, itemsPerPage = 10 }) {
   return (
     <div>
       <div className="bg-white p-8 rounded-md w-full">
-        <div className=" flex items-center justify-between pb-6">
+        <div className=" flex items-center justify-between pb-2">
           <div className="flex flex-row">
             <img
               className="w-8 h-8 ml-2"
@@ -177,39 +177,43 @@ function ShopBooking({ shopId, itemsPerPage = 10 }) {
               데이터를 저장하는데 실패했습니다.
             </div>
           )}
-          <div className="flex items-center justify-between">
-            <div className="relative text-gray-600 shadow-md rounded-3xl mr-2">
-              <input
-                type="search"
-                name="serch"
-                onChange={getQuery}
-                onKeyPress={search}
-                placeholder="이름/휴대폰 번호 뒷자리"
-                class="bg-wihte h-9 px-5 pr-10 rounded-full text-sm focus:outline-none border-2 border-gray-100"
-              />
-              <button
-                type="button"
-                onClick={search}
-                value={getQuery}
-                class="absolute right-0 top-0 mt-2.5 mr-4 bg-gray-50"
+          <div className="flex items-center justify-between"></div>
+        </div>
+        <div class=" flex items-center justify-between pb-2">
+          <div className="flex flex-row">
+            <div className="text-md text-left pt-2 ml-2">예약자명단</div>
+          </div>
+          <div className="relative text-gray-600 shadow-md rounded-3xl mr-2">
+            <input
+              type="search"
+              name="serch"
+              s
+              onChange={getQuery}
+              onKeyPress={search}
+              placeholder="이름/휴대폰 번호 뒷자리"
+              class="bg-wihte h-9 px-5 pr-10 rounded-full text-sm focus:outline-none border-2 border-gray-100"
+            />
+            <button
+              type="button"
+              onClick={search}
+              value={getQuery}
+              class="absolute right-0 top-0 mt-2.5 mr-4 bg-gray-50"
+            >
+              <svg
+                className="bg-white h-4 w-4 fill-current"
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 56.966 56.966"
+                width="512px"
+                height="512px"
               >
-                <svg
-                  className="bg-white h-4 w-4 fill-current"
-                  version="1.1"
-                  id="Capa_1"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 56.966 56.966"
-                  width="512px"
-                  height="512px"
-                >
-                  <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
-                </svg>
-              </button>
-            </div>
+                <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+              </svg>
+            </button>
           </div>
         </div>
-        <div className="text-md text-left">예약자명단</div>
         <div>
           {getBookingData && (
             <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
