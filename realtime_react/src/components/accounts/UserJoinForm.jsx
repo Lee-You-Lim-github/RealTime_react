4fc5e0499ea45527d336d2c6933bb97d24b61ced
Lee-You-Lim-github <akfl513@naver.com>
2022-03-01 17:54:47 +0900

    회원가입 폼 css 구현

--- realtime_react/src/components/accounts/UserJoinForm.jsx
+++ realtime_react/src/components/accounts/UserJoinForm.jsx
@@ -63,143 +63,169 @@ function UserJoinForm({ handleDidSave }) {
   };
 
   return (
-    <div className="mt-2">
-      <h2 className="text-2xl my-5">회원가입</h2>
-      {loading && <LoadingIndicator>가입 중...</LoadingIndicator>}
-      {error?.response?.status >= 400 && (
-        <div className="text-red-400 my-5">가입에 실패했습니다.</div>
-      )}
+    <div>
+      <div class="bg-white flex justify-center items-center">
+        <div class="lg:w-2/5 md:w-1/2 w-2/3 mt-14 mb-20">
+          <div class="bg-white p-10 rounded-lg shadow-xl mx-auto border-2 ml-14 mr-14">
+            <h1 class="text-center text-2xl mb-10 text-gray-600 ">회원가입</h1>
+            {loading && <LoadingIndicator>가입 중...</LoadingIndicator>}
+            {error?.response?.status >= 400 && (
+              <div className="text-red-400 my-5">가입에 실패했습니다.</div>
+            )}
 
-      <div>
-        <input
-          type="radio"
-          name="authority"
-          value="0"
-          onChange={handleFieldChange}
-          className="mx-1"
-        />
-        <span className="mr-28">개인</span>
-        <input
-          type="radio"
-          name="authority"
-          value="1"
-          onChange={handleFieldChange}
-          className="mx-1"
-        />
-        사업자
-      </div>
-      <p className="text-left ml-56">아이디</p>
-      <input
-        type="text"
-        name="user_id"
-        value={fieldValues.user_id}
-        onChange={handleFieldChange}
-        placeholder="영문/숫자 혼합 3자 이상 입력해주세요."
-        className="placeholder:italic placeholder:text-slate-300 border border-gray-300 rounded w-1/2 my-1 mx-2 p-2"
-      />
-      {errorMessages.user_id?.map((message, index) => (
-        <p key={index} className="text-xs text-red-400">
-          {message}
-        </p>
-      ))}
+            <div>
+              <input
+                type="radio"
+                name="authority"
+                value="0"
+                onChange={handleFieldChange}
+                className="mx-1"
+              />
+              <span className="mr-28">개인</span>
+              <input
+                type="radio"
+                name="authority"
+                value="1"
+                onChange={handleFieldChange}
+                className="mx-1"
+              />
+              사업자
+            </div>
 
-      <p className="text-left ml-56">비밀번호</p>
-      <input
-        type="password"
-        name="password"
-        value={fieldValues.password}
-        onChange={handleFieldChange}
-        placeholder="영문/숫자/특수문자 혼합 8자 이상 입력해주세요."
-        className="placeholder:italic placeholder:text-slate-300 border border-gray-300 rounded w-1/2 my-1 mx-2 p-2"
-      />
-      {errorMessages.password?.map((message, index) => (
-        <p key={index} className="text-xs text-red-400">
-          {message}
-        </p>
-      ))}
-
-      <p className="text-left ml-56">비밀번호 확인</p>
-      <input
-        type="password"
-        name="password2"
-        value={fieldValues.password2}
-        onChange={handleFieldChange}
-        placeholder="확인을 위해 동일한 비밀번호를 입력해주세요."
-        className="placeholder:italic placeholder:text-slate-300 border border-gray-300 rounded w-1/2 my-1 mx-2 p-2"
-      />
-      {errorMessages.non_field_errors?.map((message, index) => (
-        <p key={index} className="text-xs text-red-400">
-          {message}
-        </p>
-      ))}
-
-      <p className="text-left ml-56">이름</p>
-      <input
-        type="text"
-        name="username"
-        value={fieldValues.username}
-        onChange={handleFieldChange}
-        placeholder="이름을 입력해주세요."
-        className="placeholder:italic placeholder:text-slate-300 border border-gray-300 rounded w-1/2 my-1 mx-2 p-2"
-      />
-      {errorMessages.username?.map((message, index) => (
-        <p key={index} className="text-xs text-red-400">
-          {message}
-        </p>
-      ))}
-
-      <p className="text-left ml-56">닉네임</p>
-      <input
-        type="text"
-        name="nickname"
-        value={fieldValues.nickname}
-        onChange={handleFieldChange}
-        placeholder="한글 5자 이하로 입력해주세요."
-        className="placeholder:italic placeholder:text-slate-300 border border-gray-300 rounded w-1/2 my-1 mx-2 p-2"
-      />
-      {errorMessages.nickname?.map((message, index) => (
-        <p key={index} className="text-xs text-red-400">
-          {message}
-        </p>
-      ))}
-
-      <p className="text-left ml-56">휴대폰 번호</p>
-      <input
-        type="text"
-        name="telephone"
-        value={fieldValues.telephone}
-        onChange={handleFieldChange}
-        placeholder="숫자만 입력해주세요. 예)01022334567"
-        className="placeholder:italic placeholder:text-slate-300 border border-gray-300 rounded w-1/2 my-1 mx-2 p-2"
-      />
-      {errorMessages.telephone?.map((message, index) => (
-        <p key={index} className="text-xs text-red-400">
-          {message}
-        </p>
-      ))}
-      <React.Fragment>
-        <div>
-          <button
-            className="bg-violet-300 w-1/2 rounded my-1 mx-2 p-2"
-            onClick={openModal}
-          >
-            가입
-          </button>
+            <div>
+              <label class="text-gray-800 text-left font-semibold block my-3 ml-1 text-md">
+                아이디
+              </label>
+              <input
+                type="text"
+                name="user_id"
+                value={fieldValues.user_id}
+                onChange={handleFieldChange}
+                placeholder="영문/숫자 혼합 3자 이상 입력해주세요."
+                className="placeholder:italic placeholder:text-md placeholder:text-slate-300 w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+              />
+              {errorMessages?.user_id?.map((message, index) => (
+                <p key={index} className="text-xs text-red-400">
+                  {message}
+                </p>
+              ))}
+            </div>
+            <div>
+              <label class="text-gray-800 text-left font-semibold block my-3 ml-1 text-md">
+                비밀번호
+              </label>
+              <input
+                type="password"
+                name="password"
+                value={fieldValues.password}
+                onChange={handleFieldChange}
+                placeholder="영문/숫자/특수문자 혼합 8자 이상 입력해주세요."
+                className="placeholder:italic placeholder:text-md placeholder:text-slate-300 w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+              />
+              {errorMessages.password?.map((message, index) => (
+                <p key={index} className="text-xs text-red-400">
+                  {message}
+                </p>
+              ))}
+            </div>
+            <div>
+              <label class="text-gray-800 text-left font-semibold block my-3 ml-1 text-md">
+                비밀번호 확인
+              </label>
+              <input
+                type="password"
+                name="password2"
+                value={fieldValues.password2}
+                onChange={handleFieldChange}
+                placeholder="확인을 위해 동일한 비밀번호를 입력해주세요."
+                className="placeholder:italic placeholder:text-md placeholder:text-slate-300 w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+              />
+              {errorMessages.non_field_errors?.map((message, index) => (
+                <p key={index} className="text-xs text-red-400">
+                  {message}
+                </p>
+              ))}
+            </div>
+            <div>
+              <label class="text-gray-800 text-left font-semibold block my-3 ml-1 text-md">
+                이름
+              </label>
+              <input
+                type="text"
+                name="username"
+                value={fieldValues.username}
+                onChange={handleFieldChange}
+                placeholder="이름을 입력해주세요."
+                className="placeholder:italic placeholder:text-md placeholder:text-slate-300 w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+              />
+              {errorMessages.username?.map((message, index) => (
+                <p key={index} className="text-xs text-red-400">
+                  {message}
+                </p>
+              ))}
+            </div>
+            <div>
+              <label class="text-gray-800 text-left font-semibold block my-3 ml-1 text-md">
+                닉네임
+              </label>
+              <input
+                type="text"
+                name="nickname"
+                value={fieldValues.nickname}
+                onChange={handleFieldChange}
+                placeholder="한글 5자 이하로 입력해주세요."
+                className="placeholder:italic placeholder:text-md placeholder:text-slate-300 w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+              />
+              {errorMessages.nickname?.map((message, index) => (
+                <p key={index} className="text-xs text-red-400">
+                  {message}
+                </p>
+              ))}
+            </div>
+            <div>
+              <label class="text-gray-800 text-left font-semibold block my-3 ml-1 text-md">
+                휴대폰 번호
+              </label>
+              <input
+                type="text"
+                name="telephone"
+                value={fieldValues.telephone}
+                onChange={handleFieldChange}
+                placeholder="숫자만 입력해주세요. 예)01022334567"
+                className="placeholder:italic placeholder:text-md placeholder:text-slate-300 w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+              />
+              {errorMessages.telephone?.map((message, index) => (
+                <p key={index} className="text-xs text-red-400 mb-3">
+                  {message}
+                </p>
+              ))}
+            </div>
+            <React.Fragment>
+              <div>
+                <button
+                  className="text-white text-lg bg-violet-300 border-2 border-violet-300 hover:border-red-300 hover:bg-red-300 w-full rounded p-1 mb-1 mt-6"
+                  onClick={openModal}
+                >
+                  가입
+                </button>
+              </div>
+              <ConfirmModal
+                handleSubmit={handleSubmit}
+                open={modalOpen}
+                close={closeModal}
+                header="가입하시겠습니까?"
+              ></ConfirmModal>
+            </React.Fragment>
+            <div>
+              <button
+                className="text-violet-300 text-lg bg-white border-2 border-violet-300 w-full hover:border-red-300 hover:text-red-300 rounded my-1 mb-5 p-1"
+                onClick={() => navigate("/")}
+              >
+                취소
+              </button>
+            </div>
+          </div>
         </div>
-        <ConfirmModal
-          handleSubmit={handleSubmit}
-          open={modalOpen}
-          close={closeModal}
-          header="가입하시겠습니까?"
-        ></ConfirmModal>
-      </React.Fragment>
-      <div>
-        <button
-          className="bg-slate-300 w-1/2 rounded my-1 mx-2 mb-5 p-2"
-          onClick={() => navigate("/")}
-        >
-          취소
-        </button>
       </div>
     </div>
   );
