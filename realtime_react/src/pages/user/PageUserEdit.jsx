4fbaeba5b907fbb8aff476308c19a57952056a34
Lee-You-Lim-github <akfl513@naver.com>
2022-03-02 16:09:16 +0900

    회원정보 css 및 회원정보가 수정됐을 때만 로그아웃 구현

--- realtime_react/src/pages/user/PageUserEdit.jsx
+++ realtime_react/src/pages/user/PageUserEdit.jsx
@@ -1,20 +1,12 @@
 import UserEdit from "components/user/UserEdit";
-import { useAuth } from "contexts/AuthContext";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function PageUserEdit() {
   const { userId } = useParams();
-  const navigate = useNavigate();
-  const [auth, , , logout] = useAuth();
-
-  const handleLogout = () => {
-    logout();
-    navigate("/accounts/login/");
-  };
 
   return (
     <div>
-      <UserEdit userId={userId} handleDidSave={() => handleLogout()} />
+      <UserEdit userId={userId} />
     </div>
   );
 }
