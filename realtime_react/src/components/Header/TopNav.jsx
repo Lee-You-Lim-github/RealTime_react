9935af74108565a8b29492a13f2747425e53cea7
JunSoo Lee <ljsay1113@naver.com>
2022-03-16 17:39:38 +0900

    TopNav 상단 고정/스크롤 시 색 변경/메뉴 수정

--- realtime_react/src/components/Header/TopNav.jsx
+++ realtime_react/src/components/Header/TopNav.jsx
@@ -45,72 +45,96 @@ function TopNav() {
     navigate("/shop/new/");
   };
 
+  var nav = document.getElementsByClassName("navigation");
+
+  window.onscroll = function sticky() {
+    if (window.pageYOffset > nav[0].offsetTop) {
+      nav[0].classList.add("nav");
+    } else {
+      nav[0].classList.remove("nav");
+    }
+  };
+
   return (
     <div className="bg-red-200">
       <div className="header">
-        <div className="flex place-content-between gap-3 border-b-4 border-red-300">
-          <NavLink to="/" className="px-4 py-3 mt-2 text-4xl">
-            <img src={Logo} alt="LOGO" className="w-28 h-10" />
-          </NavLink>
-          <div className="flex text-xl mr-2">
-            {/* 비회원이 로그인 시 */}
-            {!auth.isLoggedIn && (
-              <>
-                <span>
-                  <button onClick={handleLogin} className={baseClassName}>
-                    예약현황
-                  </button>
-                </span>
-                <MyLink to="/accounts/login/">로그인</MyLink>
-              </>
-            )}
-            {/* 회원이 로그인 시 */}
-            {auth.isLoggedIn &&
-              !auth.is_superuser &&
-              authority_topnavi(auth.authority) && (
+        <div className="navigation">
+          <div className="flex place-content-between gap-3 ">
+            <NavLink to="/" className="px-4 py-3 mt-2 text-4xl">
+              <img src={Logo} alt="LOGO" className="w-28 h-10" />
+            </NavLink>
+            <div className="flex text-xl mr-2">
+              {/* 비회원이 로그인 시 */}
+              {!auth.isLoggedIn && (
                 <>
-                  <MyLink to={`/user/mypage/${data?.id}/`}>
-                    {auth.nickname} 님
-                  </MyLink>
-                  <MyLink to={`/user/${data?.id}/bookings/`}>예약현황</MyLink>
-                  <span>
-                    <button onClick={handleLogout} className={baseClassName}>
-                      로그아웃
-                    </button>
-                  </span>
+                  <MyLink to="/accounts/login/">로그인</MyLink>
                 </>
               )}
-            {/* 사엽자가 로그인 시 */}
-            {auth.isLoggedIn &&
-              !auth.is_superuser &&
-              !authority_topnavi(auth.authority) && (
-                <>
-                  <MyLink to={`/user/mypage/${data?.id}/`}>
-                    {auth.nickname} 님
-                  </MyLink>
+              {/* 회원이 로그인 시 */}
+              {auth.isLoggedIn &&
+                !auth.is_superuser &&
+                authority_topnavi(auth.authority) && (
+                  <>
+                    <MyLink to={`/user/mypage/${data?.id}/`}>
+                      {auth.nickname} 님
+                    </MyLink>
+                    <MyLink to={`/user/${data?.id}/bookings/`}>
+                      예약/대기현황
+                    </MyLink>
+                    <span>
+                      <button onClick={handleLogout} className={baseClassName}>
+                        로그아웃
+                      </button>
+                    </span>
+                  </>
+                )}
+              {/* 사엽자가 로그인 시 */}
+              {auth.isLoggedIn &&
+                !auth.is_superuser &&
+                !authority_topnavi(auth.authority) && (
+                  <>
+                    <MyLink to={`/user/mypage/${data?.id}/`}>
+                      {" "}
+                      {/* 관리자 대쉬보드 이동으로 변경하기 */} {auth.nickname}{" "}
+                      님
+                    </MyLink>
 
-                  {/* 매장이 등록되지 않은 경우 */}
-                  {data?.shop_set.length === 0 && (
-                    <>
-                      <MyLink to={`/shop/new/`}>가맹점 가입</MyLink>
-                      <span>
-                        <button onClick={handleJoin} className={baseClassName}>
+                    {/* 매장이 등록되지 않은 경우 */}
+                    {/* {data?.shop_set.length === 0 && (
+                      <>
+                        <MyLink to={`/shop/new/`}>가맹점 가입</MyLink>
+                        <span>
+                          <button
+                            onClick={handleJoin}
+                            className={baseClassName}
+                          >
+                            마이스토어
+                          </button>
+                        </span>
+                      </>
+                    )} */}
+                    {/* 매장이 등록된 경우 */}
+                    {/* {data?.shop_set.length === 1 && (
+                      <>
+                        <MyLink to={`/shop/${data?.shop_set[0]}/bookings/`}>
+                          매장 예약현황
+                        </MyLink>
+                        <MyLink to={`/shop/myshop/${data?.shop_set[0]}/`}>
                           마이스토어
-                        </button>
-                      </span>
-                    </>
-                  )}
-                  {/* 매장이 등록된 경우 */}
-                  {data?.shop_set.length === 1 && (
-                    <>
-                      <MyLink to={`/shop/${data?.shop_set[0]}/bookings/`}>
-                        매장 예약현황
-                      </MyLink>
-                      <MyLink to={`/shop/myshop/${data?.shop_set[0]}/`}>
-                        마이스토어
-                      </MyLink>
-                    </>
-                  )}
+                        </MyLink>
+                      </>
+                    )} */}
+                    <span>
+                      <button onClick={handleLogout} className={baseClassName}>
+                        로그아웃
+                      </button>
+                    </span>
+                  </>
+                )}
+              {/* 관리자로 로그인 */}
+              {auth.isLoggedIn && auth.is_superuser && (
+                <>
+                  <MyLink to="/admin/user/">관리자님</MyLink>
                   <span>
                     <button onClick={handleLogout} className={baseClassName}>
                       로그아웃
@@ -118,19 +142,7 @@ function TopNav() {
                   </span>
                 </>
               )}
-            {/* 관리자로 로그인 */}
-            {auth.isLoggedIn && auth.is_superuser && (
-              <>
-                <MyLink to="/admin/user/">회원관리</MyLink>
-                <MyLink to="/admin/shop/">매장관리</MyLink>
-                <MyLink to="/admin/booking/">예약관리</MyLink>
-                <span>
-                  <button onClick={handleLogout} className={baseClassName}>
-                    로그아웃
-                  </button>
-                </span>
-              </>
-            )}
+            </div>
           </div>
         </div>
       </div>
