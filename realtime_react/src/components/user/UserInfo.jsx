69d96c369b03d325014bde533bdded3da09dc97d
Eunji Nam <godangel31@gmail.com>
2022-03-17 11:29:10 +0900

    UserDashboard 내정보 표출

--- realtime_react/src/components/user/UserInfo.jsx
+++ realtime_react/src/components/user/UserInfo.jsx
@@ -1,23 +1,19 @@
 import { useApiAxios } from "api/base";
 import { useAuth } from "contexts/AuthContext";
-import React, { useEffect, useState } from "react";
-import LoadingIndicator from "components/LoadingIndicator";
-import UserInfoComponent from "./UserInfoComponent";
+import React, { useEffect } from "react";
 import "./UserInfo.css";
-import UserReviewList from "./UserReviewList";
-import mypage_icon from "assets/img/mypage_icon.png";
 
-function UserInfo({ userId }) {
+function UserInfo() {
   const [auth] = useAuth();
 
-  const [reviewList, setReviewList] = useState([]);
+  // const [reviewList, setReviewList] = useState([]);
 
-  const [reviewId, setReviewId] = useState(0);
+  // const [reviewId, setReviewId] = useState(0);
 
-  // confirm 모달창
-  const [modalOpen, setModalOpen] = useState(false);
+  // // confirm 모달창
+  // const [modalOpen, setModalOpen] = useState(false);
 
-  const [{ data: userData, loading, error }, refetch] = useApiAxios(
+  const [{ data: userData }, refetch] = useApiAxios(
     {
       url: `/accounts/api/users/${auth.id}/`,
       method: "GET",
@@ -32,92 +28,39 @@ function UserInfo({ userId }) {
     refetch();
   }, []);
 
-  const [
-    { data: reviewData, loading: reviewLoading, error: reviewError },
-    reviewRefetch,
-  ] = useApiAxios(
-    {
-      url: `/shop/api/reviews/?all`,
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${auth.access}`,
-      },
-    },
-    { manual: true }
-  );
+  // const [{ loading: deleteLoading, error: deleteError }, deleteBooking] =
+  //   useApiAxios(
+  //     {
+  //       url: `/shop/api/reviews/${reviewData?.id}/`,
+  //       method: "DELETE",
+  //       headers: {
+  //         Authorization: `Bearer ${auth.access}`,
+  //       },
+  //     },
+  //     { manual: true }
+  //   );
 
-  useEffect(() => {
-    const savedReview = reviewData?.filter(
-      (user_review) => parseInt(userId) === user_review.user_id.id
-    );
+  // const handleDelete = (e) => {
+  //   deleteBooking({
+  //     url: `/shop/api/reviews/${reviewId}/`,
+  //     method: "DELETE",
+  //   });
+  //   alert("삭제되었습니다.");
 
-    if (savedReview !== undefined) {
-      setReviewList(savedReview);
-    }
-  }, [reviewData, userId]);
+  //   window.location.replace(`/user/mypage/${userId}/`);
+  // };
 
-  const [{ loading: deleteLoading, error: deleteError }, deleteBooking] =
-    useApiAxios(
-      {
-        url: `/shop/api/reviews/${reviewData?.id}/`,
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${auth.access}`,
-        },
-      },
-      { manual: true }
-    );
-
-  const handleDelete = (e) => {
-    deleteBooking({
-      url: `/shop/api/reviews/${reviewId}/`,
-      method: "DELETE",
-    });
-    alert("삭제되었습니다.");
-
-    window.location.replace(`/user/mypage/${userId}/`);
-  };
-
-  useEffect(() => {
-    reviewRefetch();
-  }, []);
-
-  // confirm 모달 열기
-  const openModal = (e) => {
-    setModalOpen(true);
-    setReviewId(e.target.value);
-  };
-
-  // confirm 모달 닫기
-  const closeModal = () => {
-    setModalOpen(false);
-  };
+  // useEffect(() => {
+  //   reviewRefetch();
+  // }, []);
 
   return (
-    <div className="w-[900px] mx-auto">
-      <div className="flex flex-row mt-14">
-        <img src={mypage_icon} alt="" className="w-8 h-8" />
-        <h1 className="text-left text-2xl ml-2">마이페이지</h1>
-        {(loading || reviewLoading) && (
-          <LoadingIndicator>로딩 중...</LoadingIndicator>
-        )}
-        {deleteLoading && <LoadingIndicator>삭제 중...</LoadingIndicator>}
-        {deleteError?.response?.status >= 400 && (
-          <div className="text-red-400">삭제에 실패했습니다.</div>
-        )}
-      </div>
-
-      <div className="mb-20">
-        {userData && <UserInfoComponent userData={userData} auth={auth} />}
-
-        <UserReviewList
-          reviewList={reviewList}
-          openModal={openModal}
-          closeModal={closeModal}
-          handleDelete={handleDelete}
-          modalOpen={modalOpen}
-        />
-      </div>
+    <div className="ml-3">
+      <p className="text-left">ID : {auth.user_id}</p>
+      <p className="text-left">이름 : {auth.username}</p>
+      <p className="text-left">닉네임 : {auth.nickname}</p>
+      <p className="text-left">휴대전화번호 : {auth.telephone}</p>
+      <p className="text-left">노쇼횟수 : </p>
     </div>
   );
 }
