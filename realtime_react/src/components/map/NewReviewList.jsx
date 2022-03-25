0d3a083f511573778b769224ce9d45e4c8ade0d5
JunSoo Lee <ljsay1113@naver.com>
2022-03-25 14:27:36 +0900

    리뷰 리스트 수정

--- realtime_react/src/components/map/NewReviewList.jsx
+++ realtime_react/src/components/map/NewReviewList.jsx
@@ -19,46 +19,18 @@ function NewReviewList({ reviewData }) {
       }}
     >
       <ShopCarousel show={3}>
-        <div>
-          <div style={{ padding: 8 }}>
-            <div>{sortReviewData[0].rating}</div>
-            <div>{sortReviewData[0].content}</div>
-            <div>{sortReviewData[0].book_id?.user_id.nickname}</div>
-            <div>{sortReviewData[0].wait_id?.user_id.nickname}</div>{" "}
+        {sortReviewData.map((data, index) => (
+          <div>
+            <div>
+              <div style={{ padding: 8 }}>
+                <div>{data.rating}</div>
+                <div>{data.content}</div>
+                <div>{data.book_id?.user_id.nickname}</div>
+                <div>{data.wait_id?.user_id.nickname}</div>{" "}
+              </div>
+            </div>
           </div>
-        </div>
-        <div>
-          <div style={{ padding: 8 }}>
-            <div>{sortReviewData[1].rating}</div>
-            <div>{sortReviewData[1].content}</div>
-            <div>{sortReviewData[1].book_id?.user_id.nickname}</div>
-            <div>{sortReviewData[1].wait_id?.user_id.nickname}</div>{" "}
-          </div>
-        </div>
-        <div>
-          <div style={{ padding: 8 }}>
-            <div>{sortReviewData[2].rating}</div>
-            <div>{sortReviewData[2].content}</div>
-            <div>{sortReviewData[2].book_id?.user_id.nickname}</div>
-            <div>{sortReviewData[2].wait_id?.user_id.nickname}</div>{" "}
-          </div>
-        </div>
-        <div>
-          <div style={{ padding: 8 }}>
-            <div>{sortReviewData[3].rating}</div>
-            <div>{sortReviewData[3].content}</div>
-            <div>{sortReviewData[3].book_id?.user_id.nickname}</div>
-            <div>{sortReviewData[3].wait_id?.user_id.nickname}</div>{" "}
-          </div>
-        </div>
-        <div>
-          <div style={{ padding: 8 }}>
-            <div>{sortReviewData[4].rating}</div>
-            <div>{sortReviewData[4].content}</div>
-            <div>{sortReviewData[4].book_id?.user_id.nickname}</div>
-            <div>{sortReviewData[4].wait_id?.user_id.nickname}</div>{" "}
-          </div>
-        </div>
+        ))}
       </ShopCarousel>
     </div>
   );
