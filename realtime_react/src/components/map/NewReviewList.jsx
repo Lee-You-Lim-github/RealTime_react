35a9b0845d5d69e30bd610d721ed658846dd215c
JunSoo Lee <ljsay1113@naver.com>
2022-03-25 10:31:02 +0900

    리뷰 캐러셀 적용

--- realtime_react/src/components/map/NewReviewList.jsx
+++ realtime_react/src/components/map/NewReviewList.jsx
@@ -1,25 +1,65 @@
 import DebugStates from "components/DebugStates";
+import ShopCarousel from "components/map/ShopCarousel";
 
 function NewReviewList({ reviewData }) {
+  const sortReviewData = reviewData
+    .sort(
+      (newShop, newShop2) =>
+        new Date(newShop2.created_at) - new Date(newShop.created_at)
+    )
+    .slice(0, 5);
+
   return (
-    <div>
-      {reviewData
-        .sort(
-          (newShop, newShop2) =>
-            new Date(newShop2.created_at) - new Date(newShop.created_at)
-        )
-        .slice(0, 5)
-        .map((data) => {
-          return (
-            <div>
-              <div>{data.rating}</div>
-              <div>{data.content}</div>
-              <div>{data.book_id?.user_id.nickname}</div>
-              <div>{data.wait_id?.user_id.nickname}</div>
-            </div>
-          );
-        })}
-      <DebugStates reviewData={reviewData} />
+    <div
+      style={{
+        maxWidth: 1200,
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: 64,
+      }}
+    >
+      <ShopCarousel show={3}>
+        <div>
+          <div style={{ padding: 8 }}>
+            <div>{sortReviewData[0].rating}</div>
+            <div>{sortReviewData[0].content}</div>
+            <div>{sortReviewData[0].book_id?.user_id.nickname}</div>
+            <div>{sortReviewData[0].wait_id?.user_id.nickname}</div>{" "}
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <div>{sortReviewData[1].rating}</div>
+            <div>{sortReviewData[1].content}</div>
+            <div>{sortReviewData[1].book_id?.user_id.nickname}</div>
+            <div>{sortReviewData[1].wait_id?.user_id.nickname}</div>{" "}
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <div>{sortReviewData[2].rating}</div>
+            <div>{sortReviewData[2].content}</div>
+            <div>{sortReviewData[2].book_id?.user_id.nickname}</div>
+            <div>{sortReviewData[2].wait_id?.user_id.nickname}</div>{" "}
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <div>{sortReviewData[3].rating}</div>
+            <div>{sortReviewData[3].content}</div>
+            <div>{sortReviewData[3].book_id?.user_id.nickname}</div>
+            <div>{sortReviewData[3].wait_id?.user_id.nickname}</div>{" "}
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <div>{sortReviewData[4].rating}</div>
+            <div>{sortReviewData[4].content}</div>
+            <div>{sortReviewData[4].book_id?.user_id.nickname}</div>
+            <div>{sortReviewData[4].wait_id?.user_id.nickname}</div>{" "}
+          </div>
+        </div>
+      </ShopCarousel>
     </div>
   );
 }
