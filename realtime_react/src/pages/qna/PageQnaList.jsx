7a6e8fd9f97892353816d45a90bd2d4a6db4dcf5
Eunji Nam <godangel31@gmail.com>
2022-03-25 18:24:54 +0900

    User가 사용하는 페이지에 Sidebar 추가

--- realtime_react/src/pages/qna/PageQnaList.jsx
+++ realtime_react/src/pages/qna/PageQnaList.jsx
@@ -1,13 +1,20 @@
 import QnaList from "components/qna/QnaList";
+import PageUserSidebar from "pages/user/PageUserSidebar";
 import { useParams } from "react-router-dom";
 
 function PageQnaList() {
   const { userId } = useParams();
+
   return (
-    <div>
-      <h1>1:1문의</h1>
-      <div>
-        <QnaList userId={userId} />
+    <div className="grid grid-cols-8">
+      <div className="grid col-span-2">
+        <PageUserSidebar />
+      </div>
+      <div className="grid col-span-5 auto-rows-max">
+        <h1>1:1문의</h1>
+        <div>
+          <QnaList userId={userId} />
+        </div>
       </div>
     </div>
   );
