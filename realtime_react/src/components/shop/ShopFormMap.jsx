2c6a4caec0c4e43b61cb095592637180c87132e4
Lee-You-Lim-github <akfl513@naver.com>
2022-03-27 08:55:11 +0900

    매장 폼 지도 shopData get 요청 error 해결

--- realtime_react/src/components/shop/ShopFormMap.jsx
+++ realtime_react/src/components/shop/ShopFormMap.jsx
@@ -1,37 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
-function ShopFormMap({ getShopData, setFieldValues }) {
+function ShopFormMap({ getShopData, setFieldValues, refetch }) {
+  useEffect(() => {}, [getShopData]);
+
+  // 위치(위,경도)
   const [position, setPosition] = useState(
     getShopData
-      ? {
+      ? { lat: getShopData.lat, lng: getShopData.longitude }
+      : {
           lat: 36.337490378182764,
           lng: 127.44915430991462,
         }
-      : { lat: getShopData.lat, lng: getShopData.longitude }
   );
+
+  // 도로명 주소
+  const [roadAddress, setRoadAddress] = useState("");
+
+  // 주소
   const [address, setAddress] = useState("");
-  const [address1, setAddress1] = useState("");
-  //   setFieldValues({ lat: position.lat });
-  //   setFieldValues({ longitude: position.lng });
-  //   console.log(position);
-
-  //   if (position) {
-  //     console.log(position.lat);
-  //     // return setFieldValues({ lat: position.lat });
-  //   } else {
-  //     console.log("빽");
-  //   }
-  //   //   console.log(fieldValues);
-
-  //   let lat = 36.337490378182764;
-  //   let lng = 127.44915430991462;
 
   const clicked_position = (_t, mouseEvent) => {
     setPosition({
       lat: mouseEvent.latLng.getLat(),
       lng: mouseEvent.latLng.getLng(),
     });
+    console.log(position);
+
     setFieldValues((prev) => {
       return { ...prev, lat: position.lat, longitude: position.lng };
     });
@@ -43,11 +38,12 @@ function ShopFormMap({ getShopData, setFieldValues }) {
       let coord = new window.kakao.maps.LatLng(lat, lng);
 
       let callback = function (result, status) {
-        console.log(status);
         if (status === window.kakao.maps.services.Status.OK) {
           console.log("주소 :", result);
-          setAddress(result[0].road_address.address_name);
-          setAddress1(result[0].address.address_name);
+          console.log("도로명 주소:", result[0].road_address.address_name);
+          console.log("걍 주소:", result[0].address.address_name);
+          setRoadAddress(result[0].road_address.address_name);
+          setAddress(result[0].address.address_name);
         }
       };
 
@@ -59,11 +55,14 @@ function ShopFormMap({ getShopData, setFieldValues }) {
   return (
     <div>
       <Map // 지도를 표시할 Container
-        center={{
-          // 지도의 중심좌표
-          lat: 36.337490378182764,
-          lng: 127.44915430991462,
-        }}
+        center={
+          getShopData
+            ? { lat: getShopData.lat, lng: getShopData.longitude }
+            : {
+                lat: 36.337490378182764,
+                lng: 127.44915430991462,
+              }
+        }
         style={{
           width: "100%",
           height: "450px",
@@ -74,15 +73,10 @@ function ShopFormMap({ getShopData, setFieldValues }) {
         {position && <MapMarker position={position} />}
       </Map>
       {position && (
-        <p>
-          {/* {"클릭한 위치의 위도는 " +
-            position.lat +
-            " 이고, 경도는 " +
-            position.lng +
-            " 입니다"} */}
-          {`클릭한 위치는 ${address}입니다.`}
-          {`클릭한 위치는 ${address1}입니다.`}
-        </p>
+        <>
+          <p>{`클릭한 도로명 주소는 ${roadAddress}입니다.`}</p>
+          <p>{`클릭한 주소는 ${address}입니다.`}</p>
+        </>
       )}
     </div>
   );
