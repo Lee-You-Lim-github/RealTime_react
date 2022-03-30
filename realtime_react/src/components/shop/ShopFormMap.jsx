eb0ccfec917da38382eda458d4ef06269a886350
Lee-You-Lim-github <akfl513@naver.com>
2022-03-30 16:57:46 +0900

    도로명 주소만 이전 주소의 위/경도 주소를 보여주는 에러 해결 및 코드 정리

--- realtime_react/src/components/shop/ShopFormMap.jsx
+++ realtime_react/src/components/shop/ShopFormMap.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 function ShopFormMap({ getShopData, setFieldValues, refetch, shopId }) {
@@ -28,37 +28,34 @@ function ShopFormMap({ getShopData, setFieldValues, refetch, shopId }) {
   // 주소
   const [address, setAddress] = useState("");
 
-  const clicked_position = (_t, mouseEvent) => {
-    setPosition({
-      lat: mouseEvent.latLng.getLat(),
-      lng: mouseEvent.latLng.getLng(),
-    });
-    console.log(position);
+  const handleGeocode = useCallback((result, status) => {
+    if (status === window.kakao.maps.services.Status.OK) {
+      const road_address = result[0].road_address?.address_name || "";
+      const address = result[0].address?.address_name || "";
+      console.log("결과 :", result);
+      console.log("도로명 주소:", road_address);
+      console.log("지번 주소:", address);
+      setRoadAddress(road_address);
+      setAddress(address);
+    }
+  }, []);
 
+  useEffect(() => {
     setFieldValues((prev) => {
       return { ...prev, lat: position.lat, longitude: position.lng };
     });
 
     // 위도, 경도를 받으면 주소(주소, 도로명)로 변경
-    const getAddr = (lat, lng) => {
-      let geocoder = new window.kakao.maps.services.Geocoder();
+    const geocoder = new window.kakao.maps.services.Geocoder();
+    geocoder.coord2Address(position.lng, position.lat, handleGeocode);
+  }, [position, setFieldValues, handleGeocode]);
 
-      let coord = new window.kakao.maps.LatLng(lat, lng);
-
-      let callback = function (result, status) {
-        if (status === window.kakao.maps.services.Status.OK) {
-          console.log("주소 :", result);
-          console.log("도로명 주소:", result[0].road_address.address_name);
-          console.log("걍 주소:", result[0].address.address_name);
-          setRoadAddress(result[0].road_address.address_name);
-          setAddress(result[0].address.address_name);
-        }
-      };
-
-      geocoder.coord2Address(coord.getLng(), coord.getLat(), callback);
-    };
-    getAddr(position.lat, position.lng);
-  };
+  const handleMapClick = useCallback((map, mouseEvent) => {
+    setPosition({
+      lat: mouseEvent.latLng.getLat(),
+      lng: mouseEvent.latLng.getLng(),
+    });
+  }, []);
 
   return (
     <div>
@@ -76,7 +73,7 @@ function ShopFormMap({ getShopData, setFieldValues, refetch, shopId }) {
           height: "450px",
         }}
         level={3} // 지도의 확대 레벨
-        onClick={(_t, mouseEvent) => clicked_position(_t, mouseEvent)}
+        onClick={handleMapClick}
       >
         {position && <MapMarker position={position} />}
       </Map>
