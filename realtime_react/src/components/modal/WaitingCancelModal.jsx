52f50be10b9898ffa2e6e896b329549562c89007
Eunji Nam <godangel31@gmail.com>
2022-03-31 16:04:19 +0900

    회원의 대기 취소시의 모달창 구현

---
+++ realtime_react/src/components/modal/WaitingCancelModal.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function WaitingCancelModal(props) {
+  // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
+  const { open, close, header, handleCancle } = props;
+
+  const handleYes = (e) => {
+    handleCancle(e);
+    close();
+  };
+
+  return (
+    // 모달이 열릴때 openModal 클래스가 생성된다.
+    <div className={open ? "openModal modal" : "modal"}>
+      {open ? (
+        <section>
+          <header>
+            <div className="text-lg text-gray-700">{header}</div>
+            <button className="close" onClick={close}>
+              {" "}
+              &times;{" "}
+            </button>
+          </header>
+          <main>{props.children}</main>
+          <div className="flex justify-center mr-5 my-1">
+            <button
+              type="button"
+              name="yes"
+              onClick={handleYes}
+              className="mr-3 text-sm bg-violet-400 hover:bg-red-300 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+            >
+              예
+            </button>
+
+            <button
+              type="button"
+              name="no"
+              onClick={close}
+              className="text-sm border-2 border-violet-400 hover:border-red-300 hover:text-red-300 text-violet-400 py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+            >
+              아니오
+            </button>
+          </div>
+        </section>
+      ) : null}
+    </div>
+  );
+}
+
+export default WaitingCancelModal;
