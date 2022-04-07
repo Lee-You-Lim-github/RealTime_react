ae9d212770c8078c2f35305848c75a5626dd9734
Eunji Nam <godangel31@gmail.com>
2022-04-07 17:31:32 +0900

    대기 입장시 리뷰작성할 수 있도록 버튼 추가 및 대기내역 카드로 구현

--- realtime_react/src/components/user/UserWaitingList.jsx
+++ realtime_react/src/components/user/UserWaitingList.jsx
@@ -2,6 +2,7 @@ import { useApiAxios } from "api/base";
 import { useAuth } from "contexts/AuthContext";
 import { useEffect, useState } from "react";
 import ShowMore from "react-show-more-list";
+import waitinglist from "assets/img/waitinglist.png";
 import UserWaitingComponent from "./UserWaitingComponent";
 
 function UserWaiting() {
@@ -39,17 +40,21 @@ function UserWaiting() {
   };
 
   return (
-    <div>
-      <h2>대기내역</h2>
+    <div className="w-full mx-auto mb-20">
+      <div className="flex flex-row my-10">
+        <img src={waitinglist} alt="waitinglist" className="w-9 h-9 ml-7" />
+        <h2 className="text-left text-2xl ml-2">대기내역</h2>
+      </div>
+
       <ShowMore items={waitingArray} by={3}>
         {({ current, onMore }) => (
           <>
             {waitingArray.length > 0 ? (
-              <>
+              <div className="grid grid-cols-3">
                 {current?.map((wait_obj) => (
                   <UserWaitingComponent key={wait_obj.id} wait_obj={wait_obj} />
                 ))}
-              </>
+              </div>
             ) : (
               "대기 내역이 없습니다."
             )}
