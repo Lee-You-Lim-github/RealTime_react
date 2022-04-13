7c0c353e08ebfd77a2d05b300e229e16a68c7bf6
back hae ran <idg9833@gmail.com>
2022-04-13 17:09:25 +0900

    PageIndex 검색창 css 및 검색버튼 추가

--- realtime_react/src/pages/PageIndex.jsx
+++ realtime_react/src/pages/PageIndex.jsx
@@ -141,36 +141,40 @@ function PageIndex() {
         ></DirectionModal>
       </div>
       <div className="shadow-lg shadow-gray-500/80">
-        <div className="search ml-[280px] mt-6 ">
-          <div className="input-group flex flex-wrap items-stretch w-full rounded">
-            <input
-              className="w-60 h-9"
-              type="search"
+        <div className="search ml-[280px] mt-6">
+          {/* <div className="input-group flex flex-wrap items-stretch w-60 h-8 bg-white rounded hover:border-2 border-orange-400 outline-none"> */}
+          <input
+            className="w-60 h-8 mt-1 input-group flex flex-wrap items-stretch w-60 bg-white rounded focus:border-2 border-orange-400 outline-none "
+            type="search"
+            onChange={getQuery}
+            placeholder=" 매장을 검색해주세요."
+            onKeyPress={searchShop}
+          />
+          <span
+            className="input-group-text flex items-center px-3 py-1.5 text-base font-normal text-gray-700 text-center whitespace-nowrap rounded"
+            id="basic-addon2"
+          >
+            <button
+              type="button"
+              onClick={searchShop}
               onChange={getQuery}
-              placeholder="       매장을 검색해주세요."
-              onKeyPress={searchShop}
-            />
-            <span
-              className="input-group-text flex items-center px-3 py-1.5 text-base font-normal text-gray-700 text-center whitespace-nowrap rounded"
-              id="basic-addon2"
+              className="absolute right-0 top-0 mr-5"
             >
               <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="search"
-                className="w-4"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
+                className="h-8 w-4 fill-current mt-1 text-orange-400"
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 56.966 56.966"
+                width="512px"
+                height="512px"
               >
-                <path
-                  fill="currentColor"
-                  d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"
-                ></path>
+                <path d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
               </svg>
-            </span>
-          </div>
+            </button>
+          </span>
+          {/* </div> */}
         </div>
       </div>
       <div className="page flex justify-center ml-24 mt-4">
